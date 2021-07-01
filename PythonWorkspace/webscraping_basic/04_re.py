315a4cdbf295b0df713ddee05afc9d121bb13b22
HL <khl0627v@gmail.com>
2021-07-01 09:31:38 +0900

    Web Crawking & Scraping

--- PythonWorkspace/webscraping_basic/04_re.py
+++ PythonWorkspace/webscraping_basic/04_re.py
@@ -16,15 +16,15 @@ def print_match(m):
         print("m.group() :", m.group())   # 일치하는 문자열 반환
         print("m.string() :", m.string)   # 입력받은 문자열
         print("m.start() :", m.start())   # 일치하는 문자열의 시작 index
-        print("m.end() :", m.end())   # 일치하는 문자열의 끝 index
-        print("m.span() :", m.span())   # 일치하는 문자열의 시작 / 끝 index
+        print("m.end() :", m.end())       # 일치하는 문자열의 끝 index
+        print("m.span() :", m.span())     # 일치하는 문자열의 시작 / 끝 index
     else:
         print("매칭되지 않음")
 
-# m = p.match("careless")   # match : 주어진 문자열의 처음부터 일치하는지 확인   ->   care만 반환
+# m = p.match("careless")             # match : 주어진 문자열의 처음부터 일치하는지 확인   ->   care만 반환
 # print_match(m)
 
-# m = p.search("good care")   # search : 주어진 문자열 중에 일치하는 게 있는지 확인
+# m = p.search("good care")           # search : 주어진 문자열 중에 일치하는 게 있는지 확인
 # print_match(m)
 
 # lst = p.findall("good care cafe")   # findall : 일치하는 모든 것을 리스트 형태로 반환
--- PythonWorkspace/webscraping_basic/06_bs4.py
+++ PythonWorkspace/webscraping_basic/06_bs4.py
@@ -8,12 +8,12 @@ res.raise_for_status
 soup = BeautifulSoup(res.text, "lxml")
 # print(soup.title)
 # print(soup.title.get_text())
-# print(soup.a)   # soup 객체에서 처음 발견되는 a element 출력
-# print(soup.a.attrs)   # a element의 속성 정보를 출력
+# print(soup.a)           # soup 객체에서 처음 발견되는 a element 출력
+# print(soup.a.attrs)     # a element의 속성 정보를 출력
 # print(soup.a["href"])   # a element의 속성 '값' 정보를 출력
 
 # print(soup.find("a", attrs={"class":"Nbtn_upload"}))   # class="Nbtn_upload"인 a element를 찾아줘
-# print(soup.find(attrs={"class":"Nbtn_upload"}))   # class="Nbtn_upload"인 어떤 element를 찾아줘
+# print(soup.find(attrs={"class":"Nbtn_upload"}))        # class="Nbtn_upload"인 어떤 element를 찾아줘
 
 # print(soup.find("li", attrs={"class":"rank01"}))
 rank1 = soup.find("li", attrs={"class":"rank01"})
