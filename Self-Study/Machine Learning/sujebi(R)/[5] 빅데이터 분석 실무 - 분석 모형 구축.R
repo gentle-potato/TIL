bcefe2f21cafd95ddf3b3980cf306db37faa34f1
HL <khl0627v@gmail.com>
2021-11-23 19:15:36 +0900

    분석 모형 구축

---
+++ Self-Study/Machine Learning/sujebi(R)/[5] 빅데이터 분석 실무 - 분석 모형 구축.R
@@ -0,0 +1,162 @@
+# *** CH 04 분석 모형 구축 *** #
+
+
+
+
+
+
+
+##### [1] 통계 분석 #####
+
+
+
+
+
+##### <1> 주성분 분석(PCA) #####
+
+
+### (1) 주성분 분석(PCA : Principal Component Analysis)의 개념 ###
+
+# 주성분 분석 : 서로 상관성이 높은 변수를 상관관계가 없는 소수의 변수로 변환하는 '차원 축소 기법'
+# ★상관관계가 있는 고차원 자료를 자료의 변동을 최대한 보존하는 저차원 자료로 변환하는 차원 축소 방법
+
+
+### (2) 주성분의 선택 방법 ###
+
+# 누적 기여율 : 누적 기여율이 85% 이상인 지점까지를 주성분의 수로 결정
+# 스크리 산점도(Scree Plot) : 스크리 산점도의 기울기가 완만해지기 ★직전★까지의 주성분 수로 결정
+
+
+### (3) 주성분 분석 구축 프로세스 ###
+
+# ① 주성분 분석 함수 #
+
+# princomp(x, cor, scores, ...) → cor : 공분산 행렬 또는 상관 행렬(default) 사용 여부, scores : 각 주성분의 점수 계산 여부
+# 고유벡터(eigenvaetors)가 loadings 변수에 저장
+# 변수들의 선형 결합을 통해 변환된 값을 주성분 점수라고 하고, scores 변수를 통해 확인 가능
+iris_pca <- princomp(iris[ , -5],   # Species 제외
+                     cor=FALSE,
+                     scores=TRUE)
+
+summary(iris_pca)   # 누적 기여율
+
+plot(iris_pca, type="l", main="iris 스크리 산점도")   # Scree Plot
+
+# ③ 주성분 분석 절차 #
+# 주성분 분석 시각화 : biplot(x, ...)
+# → x축을 제1주성분, y축을 제2주성분으로 하고, 각 변수에 대한 주성분 적재값을 화살표로 시각화
+
+iris_pca$loadings
+# 제1주성분 점수 : 0.361*Sepal.Length + 0.857*Petal.Length + 0.358*Petal.Width
+
+iris_pca$scores   # 주성분 분석을 통해서 재표현된 모든 iris 관측 데이터의 좌표 확인
+
+biplot(iris_pca, scale=0, main="iris biplot")   # 주성분 점수 시각화화
+# 제1주성분에 포함되지 않은 Sepal.WIdth는 Comp.1과 거의 수직
+
+
+
+
+
+##### <2> 요인 분석 #####
+
+
+### (1) 요인 분석(Factor Analysis) 개념 ###
+# 요인 분석 : 변수 간의 상관관계를 고려하여 서로 유사한 변수들을 묶어 새로운 잠재요인들을 추출하는 분석 방법
+
+
+
+
+
+
+
+##### [2] 정형 데이터 분석 #####
+
+
+
+
+
+##### <1> 분류 모델 #####
+
+# ② 회귀 분석 가정 #
+# 선형성 / 독립성 / 등분산성 / 비상관성 / 정상성
+
+# ④ 회귀 분석 평가(검증) #
+# 적합도 검정 : 다변량 회귀 분석에서는 독립젼수의 수가 많아지면 결정계수(R^2)가 높아지므로
+#               독립변수가 유의하든, 유의하지 않든 독립변수의 수가 많아지면 결정계수가 높아지는 단점
+#               → 조정된 R^2 확인
+# 다중공선성(Multicollinearity) 검사 방법 : 분산팽창 요인, 상태지수
+#                                           → 분산팽창 요인(VIF : Variation  Inflation Factor)
+#                                             : 다중 회귀 모델에서 독립변수 간 상관관계가 있는지 측정하는 척도
+
+# ⑤ 회귀 분석 모형 결과 활용 #
+# plot(x, which) : x에는 선형 회귀 모형이 들어가야 함 / which : 그래프의 종류 지정(6가지 종류)
+# ----- #
+# x <- lm(Salary~PutOuts, data=Hitters)
+# plot(x, which=c(1:6))
+# ----- #
+# 그래프 유형 : 적합값 대비 잔차 그래프(Reisiduals vs Fitted) / 잔차의 Q-Q 그래프(Normal Q-Q) /
+#               표준화 잔차 그래프(Scale-Location) / 쿡의 거리(Cook's distance) /
+#               레버리지 대비 잔차 그래프(Residuals vs Leverage) / 레버리지 대비 쿡의 거리(Cook's distance vs Leverage)
+
+# 단순 선형 회귀 분석 수행 예제
+install.packages("ISLR")
+library(ISLR)
+summary(lm(Salary~PutOuts, data=Hitters))
+# 회귀식 : Salary = 0.48423*PutOuts + 395.15532
+
+# 다중 선형 회귀 분석 수행 예제
+str(Hitters)
+head(Hitters)
+summary(Hitters)   # Salary에 59개의 NA...
+
+hitters <- na.omit(Hitters)   # 결측값 제거
+summary(hitters)   # Salary의 NA 제거
+
+full_model <- lm(Salary~., data=hitters)
+summary(full_model)
+# P-Value가 유의수준 0.05보다 큰 변수들을 제거
+# DivisionW라는 더미 변수가 자동으로 생성
+
+first_model <- lm(Salary~AtBat+Hits+Walks+CWalks+Division+PutOuts,
+                  data=hitters)
+fit_model <- step(first_model, direction="backward")
+
+# vif 함수를 사용하기 위해서는 car 패키지 설치 필요
+install.packages("car")
+library(car)
+vif(fit_model)   # AtBat과 Hits 변수에서 VIF가 10보다 크기 때문에 다중공선성 문제가 심각한 것으로 해석
+
+# VIF 수치가 가장 높은 AtBat을 제거한 후 모형을 생성한 후에 다시 다중공선성 문제를 확인
+second_model <- lm(Salary~Hits+CWalks+Division+PutOuts,
+                   data=hitters)
+vif(second_model)   # 다중공선성 문제 해결
+
+summary(second_model)
+
+
+### (2) 로지스틱 회귀 분석 ###
+
+# ① 로지스틱 회귀 분석 개념 ###
+# 사후 확률(Posterior Probability) : 모형의 적합을 통해 추정된 확률
+
+# ② 변수 선택 #
+# 변수의 유의성 검정 : Z-통계량 / 모형의 유의성 검정 : 카이제곱 검정
+
+# ③ 로지스틱 회귀 분석 모형 구축 #
+# glm(formula, family, data, ...) → family : 모델에서 사용할 분포(ex. binomial)
+
+# ④ 로지스틱 회귀 분석 모형 평가 #
+
+# 혼동 행렬(Confusion Matrix)
+# confusionMatrix(data, reference, ...)
+# → data : 예측된 분류 데이터 또는 분할표(table 형식), reference : 실제 분류 데이터
+
+# AUC(AUROC : Area Under ROC)
+# ★ROC 곡선의 x축은 FPR(False Positive Ratio), y축은 TPR(True Positive Ratio)로 두고
+# 아랫부분의 면적인 AUC를 기준으로 모형을 평가
+# auc(actual, predicted, ...)
+# → actual : 정답인 label의 벡터(numeric, character 또는 factor), predicted : 예측된 값의 벡터
+# auc 함수를 처음 사용할 경우 ModelMetrics 패키지 설치 필요
+install.packages("ModelMetrics")
+library(ModelMetrics)
